8f5628d11c880c738df511942809653daa2f6d92
xiangb <binxiang.xiang@student.ecp.fr>
2017-01-06 17:02:01 +0100

    add country processing

--- processing.R
+++ processing.R
@@ -12,16 +12,11 @@ dataset <- rbind(dataset1,dataset2[-1,])
 rm(dataset1,dataset2)
 #columns names 
 colnames(dataset) <- c('age','workclass','fnlwgt','education','educationnum','mstatus','occupation','relationship','race','sex','capitalgain','capitalloss','hoursperweek','nativecountry','target')
-# format data
-dataset$age <- as.numeric(dataset$age)
-dataset$workclass <- as.factor(dataset$workclass)
-dataset$education <- as.factor(dataset$education)
-dataset$mstatus <- as.factor(dataset$mstatus)
-dataset$occupation <- as.factor(dataset$occupation)
-dataset$relationship <- as.factor(dataset$relationship)
-dataset$race <- as.factor(dataset$race)
-dataset$sex <- as.factor(dataset$sex)
-dataset$nativecountry <- as.factor(dataset$nativecountry)
+
+# remove columns fnlwgt
+
+dataset$fnlwgt <- NULL
+dataset$educationnum <- NULL
 
 # transform target value to 0 if <=50K 1 if >50K
 dataset$target <- as.character(dataset$target)
@@ -34,7 +29,58 @@ dataset$target <- as.factor(dataset$target)
 #remove every row where there is a missing value, represented by '?' in the dataset
 dataset = dataset[(dataset$workclass!=' ?'&dataset$occupation!=' ?'&dataset$nativecountry!=' ?'),]
 
-# remove columns fnlwgt, capitalgain and capitalloss
+# group nativecountry into set of countries :
+dataset$nativecountry = as.character(dataset$nativecountry)
+dataset$nativecountry[dataset$nativecountry==" Cambodia"] = "SEAsia"
+dataset$nativecountry[dataset$nativecountry==" Canada"] = "Canada"    
+dataset$nativecountry[dataset$nativecountry==" China"] = "China"       
+dataset$nativecountry[dataset$nativecountry==" Columbia"] = "LatAmerica"    
+dataset$nativecountry[dataset$nativecountry==" Cuba"] = "LatAmerica"        
+dataset$nativecountry[dataset$nativecountry==" Dominican-Republic"] = "LatAmerica"
+dataset$nativecountry[dataset$nativecountry==" Ecuador"] = "LatAmerica"     
+dataset$nativecountry[dataset$nativecountry==" El-Salvador"] = "LatAmerica" 
+dataset$nativecountry[dataset$nativecountry==" England"] = "WEurope"
+dataset$nativecountry[dataset$nativecountry==" France"] = "WEurope"
+dataset$nativecountry[dataset$nativecountry==" Germany"] = "WEurope"
+dataset$nativecountry[dataset$nativecountry==" Greece"] = "EEurope"
+dataset$nativecountry[dataset$nativecountry==" Guatemala"] = "LatAmerica"
+dataset$nativecountry[dataset$nativecountry==" Haiti"] = "LatAmerica"
+dataset$nativecountry[dataset$nativecountry==" Holand-Netherlands"] = "WEurope"
+dataset$nativecountry[dataset$nativecountry==" Honduras"] = "LatAmerica"
+dataset$nativecountry[dataset$nativecountry==" Hong"] = "China"
+dataset$nativecountry[dataset$nativecountry==" Hungary"] = "EEurope"
+dataset$nativecountry[dataset$nativecountry==" India"] = "India"
+dataset$nativecountry[dataset$nativecountry==" Iran"] = "Iran"
+dataset$nativecountry[dataset$nativecountry==" Ireland"] = "WEurope"
+dataset$nativecountry[dataset$nativecountry==" Italy"] = "WEurope"
+dataset$nativecountry[dataset$nativecountry==" Jamaica"] = "LatAmerica"
+dataset$nativecountry[dataset$nativecountry==" Japan"] = "Japan"
+dataset$nativecountry[dataset$nativecountry==" Laos"] = "SEAsia"
+dataset$nativecountry[dataset$nativecountry==" Mexico"] = "Mexico"
+dataset$nativecountry[dataset$nativecountry==" Nicaragua"] = "LatAmerica"
+dataset$nativecountry[dataset$nativecountry==" Outlying-US(Guam-USVI-etc)"] = "LatAmerica"
+dataset$nativecountry[dataset$nativecountry==" Peru"] = "LatAmerica"
+dataset$nativecountry[dataset$nativecountry==" Philippines"] = "SEAsia"
+dataset$nativecountry[dataset$nativecountry==" Poland"] = "EEurope"
+dataset$nativecountry[dataset$nativecountry==" Portugal"] = "WEurope"
+dataset$nativecountry[dataset$nativecountry==" Puerto-Rico"] = "LatAmerica"
+dataset$nativecountry[dataset$nativecountry==" Scotland"] = "WEurope"
+dataset$nativecountry[dataset$nativecountry==" South"] = "EEurope"
+dataset$nativecountry[dataset$nativecountry==" Taiwan"] = "China"
+dataset$nativecountry[dataset$nativecountry==" Thailand"] = "SEAsia"
+dataset$nativecountry[dataset$nativecountry==" Trinadad&Tobago"] = "LatAmerica"
+dataset$nativecountry[dataset$nativecountry==" United-States"] = "US"
+dataset$nativecountry[dataset$nativecountry==" Vietnam"] = "SEAsia"
+dataset$nativecountry[dataset$nativecountry==" Yugoslavia"] = "EEurope"
 
-dataset$fnlwgt <- NULL
+# format data
+dataset$age <- as.numeric(dataset$age)
+dataset$workclass <- as.factor(as.character(dataset$workclass)) # to avoid ? in levels
+dataset$education <- as.factor(dataset$education)
+dataset$mstatus <- as.factor(dataset$mstatus)
+dataset$occupation <- as.factor(as.character(dataset$occupation)) # to avoid ? in levels
+dataset$relationship <- as.factor(dataset$relationship)
+dataset$race <- as.factor(dataset$race)
+dataset$sex <- as.factor(dataset$sex)
+dataset$nativecountry <- as.factor(dataset$nativecountry)
 
