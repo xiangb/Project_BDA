eb9d63064c4ac8c2c7445df8bf54ced99ec45bab
Xiang Binxiang <binxiang.xiang@student.ecp.fr>
2017-01-14 19:33:35 +0100

    Model part ok

--- data_analysis.R
+++ data_analysis.R
@@ -13,8 +13,8 @@ plot_dist <- function(column,inch,cexnames=1.3){
        col= c("yellowgreen","skyblue"), las=1, main = column, yaxt= 'n',font=2,
        cex.lab=2, cex.axis=2, cex.main=1.5, cex.sub=1.5, cex.names = cexnames)
   co2 = format(round(cbind(co[1,], co[2,]),2))
-  text(x= plt+inch, y= c(as.integer(co2[,1])-5,as.integer(co2[,1])+5), labels=co2, pos = 2,
-       font=2, cex=2)
+  text(x= plt+inch, y= c(as.integer(co2[,1])-5,as.integer(co2[,1])+2), labels=co2, pos = 2,
+       font=2, cex=1)
 }
 
 plot_dist('race', 0.1)
@@ -23,9 +23,9 @@ legend('bottomright', c("<50k", ">50k"), fill=c("yellowgreen","skyblue"),
 plot_dist('workclass',0.2,1.2)
 plot_dist('sex',0)
 plot_dist('relationship',0)
-plot_dist('mstatus',0.3,0.9)
+plot_dist('mstatus',0.3,0.7)
 plot_dist('nativecountry',0.5,1.2)
-plot_dist('education',0.7,0.7)
+plot_dist('education',0.5,0.6)
 
 #TEST OF INDEPENDANCE
 chisq.test(table(dataset_p$target, dataset_p$race))
